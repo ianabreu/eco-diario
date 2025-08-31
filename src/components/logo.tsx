1b8df824b6125c1393ed48b46cfea38038b58288
Ian Abreu <ianabreudev@gmail.com>
2025-08-31 17:04:10 -0300

    add Link to home in logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function Logo() {
   return (
-    <div className="flex flex-row items-center gap-2">
-      <Image src={"/icon.png"} width={48} height={48} alt="Logo do site" />
-      <span className="font-bold text-base">EcoDiário</span>
-    </div>
+    <Link href="/" draggable={false}>
+      <div draggable={false} className="flex flex-row items-center gap-2">
+        <Image
+          draggable={false}
+          src={"/icon.png"}
+          width={48}
+          height={48}
+          alt="Logo do site"
+        />
+        <span draggable={false} className="font-bold text-base select-none">
+          EcoDiário
+        </span>
+      </div>
+    </Link>
   );
 }
